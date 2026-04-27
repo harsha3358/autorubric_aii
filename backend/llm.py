9ddb35cd594b2974b271173ad8ae5a8d84e6d56e
harsha3358 <harshavardhan414212@gmail.com>
2026-04-27 10:11:21 +0530

    changed llm to deploy through render

--- backend/llm.py
+++ backend/llm.py
@@ -1,11 +1,27 @@
-import ollama
+import os
+import requests
+
+HF_TOKEN = os.getenv("HF_TOKEN")
+
+API_URL = "https://api-inference.huggingface.co/models/google/gemma-2b"
+
+headers = {
+    "Authorization": f"Bearer {HF_TOKEN}"
+}
 
 def generate(prompt):
-    try:
-        response = ollama.chat(
-            model="gemma:2b",
-            messages=[{"role": "user", "content": prompt}]
-        )
-        return response["message"]["content"]
-    except Exception as e:
-        return f"LLM Error: {str(e)}"
+    payload = {
+        "inputs": prompt,
+        "parameters": {"max_new_tokens": 200}
+    }
+
+    response = requests.post(API_URL, headers=headers, json=payload)
+
+    if response.status_code != 200:
+        return "LLM Error"
+
+    data = response.json()
+
+    if isinstance(data, list):
+        return data[0].get("generated_text", "")
+    return str(data)
